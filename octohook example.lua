a377ab54822b7a9b6c18f5ea825ed8e1752a93be
Qwisaskid <110581255+Qwisaskid@users.noreply.github.com>
2022-09-11 18:34:52 -0700

    Update octohook example.lua

--- octohook example.lua
+++ octohook example.lua
@@ -1,9 +1,4 @@
---[[ 
-thx ride and frosted for the ui docs credits to liam#4567 for making the ui 
-confirm = true 
-looking into the source code helps 
-]]
-
+-- thx ride and frosted for the docs 
 local startTick = tick()
 
 local library =
@@ -15,7 +10,7 @@ local utility = library.utility
 local signal = library.signal
 
 local menu =
-    library.NewWindow({title = library.cheatname .. " | Private | " ..   library.gamename, size = UDim2.new(0, 500, 0.5, 20)}) -- also no Qw hub doesnt exist just a name 
+    library.NewWindow({title = library.cheatname .. " - Private | " ..   library.gamename, size = UDim2.new(0, 500, 0.5, 20)}) -- also no Qw hub doesnt exist just a name 
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --> Tabs <-- 
@@ -27,20 +22,26 @@ local SettingsTab = library:CreateSettingsTab(menu)
 --> sections <-- 
 local Aimbotsec = CombatTab:AddSection("Aim-Assist", 1)
 local saimsec = CombatTab:AddSection("Bullet-Prioritize", 2)
+local miscsec1 = Misctab:AddSection("tp/buy", 1)
 
 
 local aimbottoggle = Aimbotsec:AddToggle(
     {
+        state = true;
+        tooltip = 'this is a tooltip!';
+        risky = true,
         text = "Enabled",
-        flag = "Aimbot_enabled",
+        flag = "testflag",
         callback = function(bool)
-  print(bool)
         end
     }
 )
 aimbottoggle:AddBind(
     {
+        tooltip = 'this is a tooltip!';
         text = "Aimbot",
+         mode = "hold",
+          risky = true,
         flag = "Aimbotkeybind",
         nomouse = false,
         noindicator = false,
@@ -78,3 +79,30 @@ Aimbotsec:AddList(
         end
     }
 )
+--ignore this is just my da hood teleports
+miscsec1:AddList(
+    {
+        text = "Teleport",
+        flag = "",
+          open = true;
+        selected = "";
+        values = { 
+"Downhill gunz",
+"Uphill gunz",
+"Bank"
+        },
+    --   
+        callback = function(bool)
+          if bool == "Downhill gunz" then 
+          game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-578.4213256835938, 8.286005020141602, -732.426025390625)
+elseif bool == "Uphill gunz" then 
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(485.7781677246094, 48.04171371459961, -623.5838012695312)
+elseif bool == "Bank" then 
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-447.1152038574219, 22.976484298706055, -287.2018127441406)
+          end 
+        end
+    }
+)
+if library.flags.testflag then  --ignore again i was testing 
+
+end 
