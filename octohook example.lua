56b02685856a61a65b62e7c24d0658647bab89e3
Qwisaskid <110581255+Qwisaskid@users.noreply.github.com>
2022-09-12 21:34:14 -0700

    Update octohook example.lua

--- octohook example.lua
+++ octohook example.lua
@@ -1,8 +1,9 @@
--- thx ride and frosted for the docs 
+-- thx ride and frosted for the docs and also i reccomend looking into the source cuz im lazy + i probably did everything maybe idk also ignore my tp they're just for testing 
 local startTick = tick()
 
+
 local library =
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/notfrostedwow/sadasds/main/octohooksrc.lua"))(
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Qwisaskid/ui-librarys/main/octohook%20ui%20source.lua"))(
     {cheatname = "Qwhub", gamename = "Baseplate"} --
 )
 library:init()
@@ -23,17 +24,18 @@ local SettingsTab = library:CreateSettingsTab(menu)
 local Aimbotsec = CombatTab:AddSection("Aim-Assist", 1)
 local saimsec = CombatTab:AddSection("Bullet-Prioritize", 2)
 local miscsec1 = Misctab:AddSection("tp/buy", 1)
-
+local exploitssec = Misctab:AddSection("Exploits", 2)
 
 local aimbottoggle = Aimbotsec:AddToggle(
     {
-        state = true;
+      
+        state = false;
         tooltip = 'this is a tooltip!';
-        risky = true,
+        risky = false,
         text = "Enabled",
         flag = "testflag",
         callback = function(bool)
-        end
+    end 
     }
 )
 aimbottoggle:AddBind(
@@ -55,6 +57,8 @@ Aimbotsec:AddList(
     {
         text = "Bone",
         flag = "",
+         selected = {'HumanoidRootPart', "Head"};
+        multi = true,
         values = { 
             "Head",
             "UpperTorso",
@@ -79,15 +83,53 @@ Aimbotsec:AddList(
         end
     }
 )
+Aimbotsec:AddSlider(
+    {
+        text = "Sensetivity",
+        flag = '"',
+        suffix = "°",
+        min = 0,
+        max = 100,
+        increment = .1,
+        callback = function(v)
+            
+        end
+    }
+)
+local Fovvisualizetoggle = Aimbotsec:AddToggle(
+    {
+      
+        state = false;
+        tooltip = "Automaticallu aims when they are in the fov circle!";
+        risky = false,
+        text = "Visualize fov ",
+        flag = "testflag",
+        callback = function(bool)
+    end 
+    }
+)
+Fovvisualizetoggle:AddColor(
+    {
+        text = "Color",
+          color = Color3.new(1,1,1);
+          tooltip = '';
+        flag = "",
+        callback = function()
+        end
+    }
+)
 --ignore this is just my da hood teleports
 miscsec1:AddList(
     {
         text = "Teleport",
         flag = "",
+        risky = true, 
           open = true;
         selected = "";
+      
         values = { 
 "Downhill gunz",
+
 "Uphill gunz",
 "Bank"
         },
@@ -103,6 +145,30 @@ elseif bool == "Bank" then
         end
     }
 )
-if library.flags.testflag then  --ignore again i was testing 
 
-end 
+
+
+
+exploitssec:AddSeparator({text = "Goto"})
+
+exploitssec:AddBox(
+    {
+        text = "insert plr",
+        flag = "box",
+        input = '';
+         focused = true;
+        callback = function(text)
+            
+        end
+    }
+)
+exploitssec:AddButton(
+    {
+        text = "Goto",
+        risky = true,
+        confirm = true, 
+        callback = function()
+         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[library.flags.box].Character.HumanoidRootPart.CFrame 
+        end
+    }
+)
